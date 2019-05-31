fec2188f512a4347185ce44b39abfa3f4aa0fcde
James Pier <jpier2012@gmail.com>
2019-05-31 20:49:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(username: params[:username])
     session[:user_id] = @user.id if @user
+    redirect '/account'
   end
 
   get '/account' do
